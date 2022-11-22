bbc90a30114108e4ea20b8dc2f2ff6a012457b4a
Alex Oxrud <alex.oxrud@gmail.com>
2022-11-22 15:46:23 -0700

    fix: padding

--- public/index.js
+++ public/index.js
@@ -21,18 +21,6 @@ var init = function init() {
       });
     }
   });
-
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-      navigator.serviceWorker.register('/sw.js').then(function (registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function (err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-  }
 };
 
 var viewMoreProjects = function viewMoreProjects() {
