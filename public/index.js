35f4d5eca846d4b8ce287e50d0151bf2dae3000a
Alex Oxrud <alex.oxrud@gmail.com>
2022-11-23 10:15:28 -0700

    chore: updating registration

--- public/index.js
+++ public/index.js
@@ -60,3 +60,17 @@ var scrollToY = function scrollToY() {
 };
 
 init();
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      registration.update();
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
